7f540ed10beeacaeffcd6ca047157d6bae000ab2
Bekithemba Mdluli <putthetrust.gamedev@gmail.com>
2023-06-30 22:48:51 +0200

    mobile base style

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -3,8 +3,20 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
-  styleUrls: ['./product.component.css']
+  styleUrls: ['./product.component.css'],
 })
 export class ProductComponent {
+  quantity = 0;
 
+  onClick = () => {
+    console.log('clicked');
+  };
+
+  modifyCart = (value: number) => {
+    if (this.quantity + value < 0) {
+      return;
+    }
+
+    this.quantity += value;
+  };
 }
