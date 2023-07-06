b658ce44be4e9797b9698cc438637290105460c5
Bekithemba <putthetrust.gamedev@gmail.com>
2023-07-06 11:38:54 +0200

    feat: next & previous buttons functionality

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -7,9 +7,39 @@ import { Component } from '@angular/core';
 })
 export class ProductComponent {
   quantity = 0;
+  currentImage = 1;
+  thumbnails = [
+    {
+      id: 1,
+      thumbnail: '../images/image-product-1-thumbnail.jpg',
+      image: '../images/image-product-1.jpg',
+    },
+    {
+      id: 2,
+      thumbnail: '../images/image-product-2-thumbnail.jpg',
+      image: '../images/image-product-1.jpg',
+    },
+    {
+      id: 3,
+      thumbnail: '../images/image-product-3-thumbnail.jpg',
+      image: '../images/image-product-1.jpg',
+    },
+    {
+      id: 4,
+      thumbnail: '../images/image-product-4-thumbnail.jpg',
+      image: '../images/image-product-1.jpg',
+    },
+  ];
 
-  onClick = () => {
-    console.log('clicked');
+  onClick = (direction: number) => {
+    if (
+      this.currentImage + direction < 1 ||
+      this.currentImage + direction > this.thumbnails.length
+    ) {
+      return;
+    }
+    this.currentImage += direction;
+    // console.log(this.currentImage);
   };
 
   modifyCart = (value: number) => {
